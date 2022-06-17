3077ce4c4eae9ad026c072ca3fa67a3476563fba
beginner1986 <beginner1986@gmail.com>
2022-06-17 20:01:43 +0200

    Spring Boot project setup

---
+++ src/main/java/com/beginner/webauthentication/WebAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.beginner.webauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAuthenticationApplication.class, args);
+    }
+
+}
