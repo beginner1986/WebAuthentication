867c81cc2d966e7ca0d473baf10e48b443a09c3d
beginner1986 <beginner1986@gmail.com>
2022-06-25 13:53:09 +0200

    RoleRepository and UserRepository added

---
+++ src/main/java/com/beginner/webauthentication/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.beginner.webauthentication.repository;
+
+import com.beginner.webauthentication.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
