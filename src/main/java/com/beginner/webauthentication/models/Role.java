edc362967e230e71334f56c15f8c5f9c3ee39456
beginner1986 <beginner1986@gmail.com>
2022-06-25 13:46:24 +0200

    User model added

--- src/main/java/com/beginner/webauthentication/models/Role.java
+++ src/main/java/com/beginner/webauthentication/models/Role.java
@@ -10,9 +10,11 @@ import javax.persistence.*;
 @Entity
 @Table(name = "roles")
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private ERole name;
---
+++ src/main/java/com/beginner/webauthentication/models/User.java
@@ -0,0 +1,45 @@
+package com.beginner.webauthentication.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+    })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    String username;
+
+    @NotBlank
+    @Email
+    @Size(max = 20)
+    String email;
+
+    @NotBlank
+    @Size(max = 120)
+    String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<Role> roles = new HashSet<>();
+}
